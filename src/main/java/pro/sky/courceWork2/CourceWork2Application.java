519d08ad9be0e908114b348bfd08a675d4918eda
Sergey <zapravilsj@mail.ru>
2022-11-20 19:56:52 +0500

    первый коммит

---
+++ src/main/java/pro/sky/courceWork2/CourceWork2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.courceWork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourceWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourceWork2Application.class, args);
+	}
+
+}
